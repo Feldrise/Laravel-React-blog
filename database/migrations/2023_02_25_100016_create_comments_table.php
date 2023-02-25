23012f4110ac1e0e6ae342fe016ec1ce7b662cd3
Feldrise <admin@feldrise.com>
2023-02-25 12:16:15 +0100

    🗃️ Create database migrations

---
+++ database/migrations/2023_02_25_100016_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('post_id')->unsigned();
+            $table->mediumText('comment');
+            $table->tinyInteger('approved')->default(0)->comment('0=pending 1=apporoved 2=rejected');
+            $table->timestamps();
+
+            // We make the relations
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
